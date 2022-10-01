00db0ca5909b0683f6fd32e6e492c1df26c4248c
James Matherne <93159223+jmatherne65@users.noreply.github.com>
2022-10-01 14:48:37 -0500

    Squashed commit of the following:
    commit c2e96c5e9850ad0ed445d30fdc1e7742d6234de6
    Author: James Matherne <93159223+jmatherne65@users.noreply.github.com>
    Date:   Sat Oct 1 14:42:22 2022 -0500
    
        Co-authored-by: DonovanBrown <DonovanBrown@users.noreply.github.com>
        Co-authored-by: Alijah Johnson <alijahj@users.noreply.github.com>
    
    commit c146f3b0649dcf0c9783b7ea84d3f048adf7b4a4
    Author: James Matherne <93159223+jmatherne65@users.noreply.github.com>
    Date:   Sat Oct 1 14:00:25 2022 -0500
    
        commit
    
    commit 0cabbe7a9d5597abfe58db90b23bb8bfb293f892
    Author: James Matherne <93159223+jmatherne65@users.noreply.github.com>
    Date:   Sat Oct 1 13:02:35 2022 -0500
    
        began navbar

--- hackathon2022/src/App.js
+++ hackathon2022/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import { BrowserRouter as Router } from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Navbar />
+      </Router>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <element id='rcorners3'></element>
+        <element id='rcorners4'></element>
+        <buttonrow justify="center">
+          <button1 className="button1" onClick={EatMeal}>Have A Mindful Meal</button1>
+          <button2 className="button2" onClick={Exercise}>Get Some Exercise</button2>
+          <button3 className="button3" onClick={Shower}>Tend To Hygiene</button3>
+        </buttonrow>
       </header>
+
+      <body className="App-body">
+          <h1>Everybody goes through hard times. We're here to help.</h1>
+             <h3> Life can be difficult at times. Just know that you are not alone in this. Approximately 13% of all teenagers suffer from some form of depression, and 32% suffer from anxiety. </h3>
+
+
+              **We are not mental health professionals, and this advice should not be taken as a final decision. If you are suffering from mental health issues, please seek professional help.
+      </body>
+
     </div>
   );
+}
+
+
+function EatMeal() {
+  alert("Eating a proper, healthy meal can often help better your mood. When possible, please try to eat 3 times a day, and include vegetables and protein in these meals.");
+}
+
+function Exercise() {
+  alert("Getting sunlight and being active can release natural endorphins, helping you feel better. Consistent light workouts, such as jogging or biking, can better your mental health in the long term.");
+}
+
+function Shower() {
+  alert("Often, poor hygiene can feed into feelings of anxiety and self doubt. While it can be very hard to be motivated to do so, consistently taking showers and taking care of yourself can greatly improve your self image.");
 }
 
 export default App;
