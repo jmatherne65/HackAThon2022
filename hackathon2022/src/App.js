c146f3b0649dcf0c9783b7ea84d3f048adf7b4a4
James Matherne <93159223+jmatherne65@users.noreply.github.com>
2022-10-01 14:00:25 -0500

    commit

--- hackathon2022/src/App.js
+++ hackathon2022/src/App.js
@@ -1,38 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
-import {BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <Navbar />
+        <Navbar />
       </Router>
       <header className="App-header">
         <element id='rcorners3'></element>
         <element id='rcorners4'></element>
         <buttonrow justify="center">
-          <button className="button" onClick={EatMeal}>Have A Mindful Meal</button>
-          <button className="button" onClick={Exercise}>Get Some Exercise</button>
-          <button className="button" onClick={Shower}>Tend To Hygiene</button>
+          <button1 className="button1" onClick={EatMeal}>Have A Mindful Meal</button1>
+          <button2 className="button2" onClick={Exercise}>Get Some Exercise</button2>
+          <button3 className="button3" onClick={Shower}>Tend To Hygiene</button3>
         </buttonrow>
       </header>
+
+      <body>
+<div>
+  Everybody goes through tough times. Believe me, been there, done that.
+</div>
+
+      </body>
+
     </div>
   );
 }
 
+
 function EatMeal() {
-  alert("Button 1 yep");
+  alert("Eating a proper, healthy meal can often help better your mood. When possible, please try to eat 3 times a day, and include vegetables and protein in these meals.");
 }
 
 function Exercise() {
-  alert("Button 2 yep");
+  alert("Getting sunlight and being active can release natural endorphins, helping you feel better. Consistent light workouts, such as jogging or biking, can better your mental health in the long term.");
 }
 
 function Shower() {
-  alert("Button 3 yep");
+  alert("Often, poor hygiene can feed into feelings of anxiety and self doubt. While it can be very hard to be motivated to do so, consistently taking showers and taking care of yourself can greatly improve your self image.");
 }
 
 export default App;
